99e1a815d15aba837ac3eeded7fbbf96b9e76304
DimeWhite <59131481+DimeWhite@users.noreply.github.com>
2020-06-08 16:45:49 +0600

    Ok, good job, Dude v. 1.0.0

---
+++ Bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord import utils
+from discord.ext import commands
+from discord.ext.commands import bot
+from faceit_api import FaceitData
+import pyosu
+
+
+OSUToken = "2e0538f28b266e07d7b16f84c7a542056a65fc9d"
+FaceitToken = "4fe49639-5c4e-46fb-99dd-c979336a3d5c"
+DSToken = "NzE5NDg1MjA4NDUzMTg1NTg5.Xt4QfA.Sz1CrRQSJwtDqf0eRH5WP5DB30A"
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.command(pass_context=True)
+async def faceit(ctx, arg):
+    elo = faceit.player_details(arg)['games']['csgo']['faceit_elo']
+    if elo is None:
+        await ctx.send("Пользователь не найден")
+    else:
+        await ctx.send("{}\nElo = {} \nlvl =  {}".format(faceit.player_details(arg)['nickname'], elo, faceit.player_details(arg)['games']['csgo']['skill_level']))
+
+
+@bot.command(pass_context=True)
+async def osu(ctx, arg):
+    await ctx.send("{}'s pp  = {}".format(arg, osu.user(arg, 0, 'pp_raw')))
+
+
+faceit = FaceitData(FaceitToken)
+osu = pyosu.main(OSUToken)
+bot.run(DSToken)
